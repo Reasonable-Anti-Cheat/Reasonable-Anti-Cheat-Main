624959851a1922c9612b0a526d7c5f5215844f72
iop33E <93890773+iop33e@users.noreply.github.com>
2025-10-30 06:06:28 -0700

    Add files via upload

---
+++ Reasonable Anti-Cheat C# dll/Class1.cs
@@ -0,0 +1,75 @@
+﻿namespace Reasonable_Anti_Cheat;
+using System;
+using System.IO;
+
+
+public class ReasonableAntiCheat
+{
+    public string[] exampleFolders = { "Mods", "Dependencies", "Plugins", "BepInEx/plugins" };
+
+    private bool isAllowed(UInt64 reason, UInt64[] allowedReasons)
+    {
+        return allowedReasons.Contains(reason);
+    }
+    private UInt64 getChecksum(string pathpath)
+    {
+        UInt64 checksum = 0;
+        if (System.IO.File.Exists(pathpath))
+        {
+            // Old way (small files):
+            //byte[] bytesbytes = System.IO.File.ReadAllBytes(pathpath);
+            //foreach (byte b in bytesbytes) {
+            //    checksum += b;
+            //}
+            // New way (any size files):
+            FileStream thisStream = System.IO.File.OpenRead(pathpath);
+            while (thisStream.Length != thisStream.Position)
+            {
+                checksum += (UInt64)thisStream.ReadByte();
+            }
+        }
+        return checksum;
+    }
+    bool runAntiCheat(UInt64[] allowedReasons, string[] foldersToCheck, string[] fileTypesToCheck)
+    {
+        bool needsRestart = false;
+        foreach (string folder in foldersToCheck)
+        {
+            if (System.IO.Directory.Exists(folder))
+            {
+                foreach (string file in System.IO.Directory.GetFiles(folder))
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    if (fileTypesToCheck.Contains(fileInfo.Extension))
+                    {
+                        if (!isAllowed(getChecksum(file), allowedReasons))
+                        {
+                            if (!needsRestart) { needsRestart = true; }
+                            retret(file);
+                        }
+                    }
+                }
+                foreach (string subfolder in System.IO.Directory.GetDirectories(folder))
+                {
+                    foreach (string file in System.IO.Directory.GetFiles(subfolder))
+                    {
+                        FileInfo fileInfo = new FileInfo(file);
+                        if (fileTypesToCheck.Contains(fileInfo.Extension))
+                        {
+                            if (!isAllowed(getChecksum(file), allowedReasons))
+                            {
+                                if (!needsRestart) { needsRestart = true; }
+                                retret(file);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return needsRestart;
+    }
+    private void retret(string ofile) 
+    {
+        System.IO.File.Move(ofile, ofile + ".notallowedfile");
+    }
+}
